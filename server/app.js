840bb6c738ea73ec786054f7ec3d501c40c15993
Vinutha Gowde <vinutha.gowde@mphasis.com>
2026-02-10 23:01:18 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 const bluetoothService = require('./bluetooth-service');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
